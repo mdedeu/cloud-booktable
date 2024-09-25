f727bee408918c646ff08e86b5ee886e38a3f635
marquitos.eth <mdedeu@itba.edu.ar>
2024-09-25 11:30:05 -0300

    backend functions

--- backend/persistance/reservation_repository.py
+++
@@ -1,9 +0,0 @@
-class ReservationRepository:
-    def __init__(self):
-        self.reservations = []
-
-    def create_reservation(self, reservation):
-        self.reservations.append(reservation)
-
-    def get_reservations(self):
-        return self.reservations
--- backend/service/restaurant_service.py
+++
@@ -1,8 +0,0 @@
-from persistance.restaurant_repository import RestaurantRepository
-
-
-class RestaurantService:
-    def __init__(self):
-        self.repository = RestaurantRepository()
-    def add_link_to_restaurant(self, restaurant_id, link):
-        return self.repository.update_restaurant_link(restaurant_id, link)
