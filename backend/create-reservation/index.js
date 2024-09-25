0eebc729ff728128c823467b193dbed07c8285ff
marquitos.eth <mdedeu@itba.edu.ar>
2024-09-25 12:20:32 -0300

    feat: create reservation working

--- backend/create-reservation/index.js
+++ backend/create-reservation/index.js
@@ -12,11 +12,7 @@ exports.handler = async (event, context) => {
 
     try {
         await client.connect();
-        let body = {};
-        if (event.body) {
-            body = JSON.parse(event.body);
-        }
-        const res = await client.query("insert into reservations (table_id, name, email, date, time_slot, guests) values ($1, $2, $3, $4, $5, $6)", [body.table_id, body.name, body.email, body.date, body.time_slot, body.guests]);
+        const res = await client.query("insert into reservations (table_id, name, email, date, time_slot, guests) values ($1, $2, $3, $4, $5, $6)", [event.table_id, event.name, event.email, event.date, event.time_slot, event.guests]);
         await client.end();
         return {
             statusCode: 200
